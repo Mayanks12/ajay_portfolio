e7ec7bc10fba9f9d489ab271feec2d07452119e7
Mayanks12 <mayank@techstuff.cloud>
2024-01-23 10:33:43 +0530

    first commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,31 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  // {
+  //   label: 'Articles',
+  //   pathname: '/articles',
+  // },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'github',
+    url: 'https://www.figma.com/@Hamish',
+    icon: 'github',
+  },
+  {
+    label: 'LinkedIn',
+    url: 'https://github.com/HamishMW',
+    icon: 'linkedIn',
+  },
+];
